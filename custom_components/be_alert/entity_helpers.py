087e4b65e2c4a4db84732ff132418755e28c2891
Spiffo <peter.vanmever@gmail.com>
2025-11-08 22:35:35 +0100

    sensor naming change

--- custom_components/be_alert/entity_helpers.py
+++ custom_components/be_alert/entity_helpers.py
@@ -34,11 +34,6 @@ def _create_location_entities(
     if not entity_id:
         return []
 
-    state = hass.states.get(entity_id)
-    friendly_name = (
-        state.name if state and state.name else entity_id.split(".")[-1]
-    )
-
     sensor_unique_id = f"be_alert_{_slug(entity_id)}"
     config = BeAlertLocationSensorConfig(
         hass,
